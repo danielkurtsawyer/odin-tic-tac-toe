4822dfa9c232b798817466093094cf2124a42acc
danielkurtsawyer <19danielsawyer@gmail.com>
2023-10-03 18:36:50 -0400

    Begin game logic and finish board layout

---
+++ script.js
@@ -0,0 +1,32 @@
+// The gameboard will be stored as an array inside of a Gameboard Object like so:
+// index:       0   1   2 
+//              3   4   5 
+//              6   7   8
+
+// since we only need one of these, we will use a module for the gameboard
+const gameBoard = (() => {
+    const board = [];
+    const move = (symbol, index) => {
+        if(!board[index]){
+            board[index] = symbol;
+        }
+    };
+    const getBoard = () => board;
+    return {move, getBoard};
+})();
+
+// We will also want an object to control the flow of the game, which will also be a module
+const displayController = (() => {
+    let move = 1; 
+    const displayBoard = () => {
+
+    };
+    const resetBoard = () => {
+        
+    }
+})();
+
+// finally, we will want to create player objects. Since there is more than one player, this will be a factory 
+const Player = (name) => {
+    const getName = () => name; 
+}
